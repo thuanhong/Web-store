f548be7511d801b5036c821c7bbc322f10b86806
HONG Thanh Thuan <thuan.hong@f4.intek.edu.vn>
2020-01-14 16:01:13 +0700

    add: adding login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,41 @@
 const User = require('../models/users')
+const bcrypt = require('bcryptjs')
 
 exports.getLogin = (req, res, next) => {
-    console.log(req.session.isLogged)
+    const show = req.query.show
     res.render('auth/login', {
         path: '/auth/login',
         title_page: 'Login',
-        isAuthenticated: false
+        isAuthenticated: false,
+        show: show
     })
 }
 
 exports.postLogin = (req, res, next) => {
-    User.findById('5e16e64dec4afd3698972a20')
-        .then(user => {
-            req.session.user = user;
-            req.session.isLogged = true;
-            // Make sure session have been save before navigate
-            req.session.save(error => {
-                console.error(error)
-                res.redirect('/')
-            })
+    const email = req.body.email
+    const password = req.body.password
+
+    User.findOne({email: email})
+        .then(userCollection => {
+            if (userCollection) {
+                bcrypt.compare(password, userCollection.password)
+                    .then(result => {
+                        if (result === true) {
+                            req.session.user = userCollection;
+                            req.session.isLogged = true;
+                            // Make sure session have been save before navigate
+                            req.session.save(error => {
+                                console.error(error)
+                                res.redirect('/')
+                            })
+                        }
+                    })
+            } else {
+                res.redirect('/auth/login?show=Your email or password incorrect')
+            }
         })
-        .catch(error => {
-            console.error(error)
+        .catch(errror => {
+            console.error(errror)
         })
 }
 
@@ -36,14 +50,23 @@ exports.postSignUp = (req, res, next) => {
     const user = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
-    new User({
-        name: user,
-        email: email,
-        password: password
-    }).save().then(() => {
-        res.redirect('/auth/login')
-    })
-    .catch(err => {
-        console.error(err)
-    })
+    User.findOne({email: email})
+        .then(email => {
+            if (email) {
+                return res.redirect('/auth/login?show=Your email were exist')
+            }
+            return bcrypt.hash(password, 12)
+                .then(hashPassword => {
+                    new User({
+                        name: user,
+                        email: email,
+                        password: hashPassword
+                    }).save().then(() => {
+                        res.redirect('/auth/login?show=Register successfull')
+                    })
+                })
+        })
+        .catch(err => {
+            console.error(err)
+        })
 }
